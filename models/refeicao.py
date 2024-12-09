8387e3e1264e7bc0e81efbca382cb2377c0810e1
Gabriel Claudino <gaclaudino.13@gmail.com>
2024-12-09 11:51:37 -0300

    Create class refeicao

--- models/refeicao.py
+++ models/refeicao.py
@@ -1,7 +1,19 @@
-# - Nome
-# - Descrição
-# - Data e Hora
-# - Está dentro ou não da dieta
+from database import db
+from datetime import datetime
 
-class Refeicao():
-    pass
+
+class Refeicao(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80), nullable=False)
+    descricao = db.Column(db.Text, nullable=False)
+    data_hora = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    dentro_da_dieta = db.Column(db.Boolean, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "descricao": self.descricao,
+            "data_hora": self.data_hora.isoformat() if self.data_hora else None,
+            "dentro_da_dieta": self.dentro_da_dieta,
+        }
