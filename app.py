ee1fe6d96c84ac106ab61910f7c3fa931994aa99
Gabriel Claudino <gaclaudino.13@gmail.com>
2024-12-09 11:52:02 -0300

    feat: add db

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from database import db
+from models.refeicao import Refeicao
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+db.init_app(app=app)
 
 @app.route("/", methods=['GET'])
 def welcome():
