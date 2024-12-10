74ef60ee38e7804535ecb80b6c31d53e88a27586
Gabriel Claudino <gaclaudino.13@gmail.com>
2024-12-10 14:52:15 -0300

    fix: o nome é unico

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
 
     # Relacionamento com Refeicao
